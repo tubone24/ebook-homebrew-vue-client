e8d6db6cfe85ca58bf5359b7234167b4f6ef106d
tubone24 <tubo.yyyuuu@gmail.com>
2019-07-13 14:06:16 +0900

    fix source

--- config/index.js
+++ config/index.js
@@ -49,8 +49,8 @@ module.exports = {
 
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
-    assetsSubDirectory: '',
-    assetsPublicPath: 'static',
+    assetsSubDirectory: 'static',
+    assetsPublicPath: '/',
 
     /**
      * Source Maps
